926c2dfc2d7efea4a19484326faf70dd880ac551
Dylan Walls <dylanwalls@dylans-mbp-2.home>
2023-06-28 16:27:17 +0100

    Update to only trigger when assigned to Maintenance

--- index.js
+++ index.js
@@ -6,34 +6,38 @@ const fetch = require('isomorphic-fetch');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false })); // Use body-parser to parse URL-encoded data
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/webhook', async (req, res) => {
   const ticket = req.body;
-  console.log(ticket);
   const message = ticket.message ? ticket.message : 'New ticket received';
 
   console.log('Received ticket:', ticket); // Log the received ticket object
   console.log('Message:', message); // Log the message
 
-  try {
-    await sendWhatsAppMessage(message);
-    res.json({ success: true, message: 'WhatsApp message sent successfully', ticket });
-  } catch (error) {
-    console.error('Failed to send WhatsApp message:', error);
-    res.status(500).json({ success: false, message: 'Failed to send WhatsApp message' });
+  // Check if the ticket is assigned to the specific team
+  if (ticket.team_id === '346034') {
+    try {
+      await sendWhatsAppMessage(message);
+      res.json({ success: true, message: 'WhatsApp message sent successfully', ticket });
+    } catch (error) {
+      console.error('Failed to send WhatsApp message:', error);
+      res.status(500).json({ success: false, message: 'Failed to send WhatsApp message' });
+    }
+  } else {
+    res.json({ success: true, message: 'Ticket not assigned to the specific team' });
   }
 });
 
 async function sendWhatsAppMessage(message) {
   console.log('Calling sendWhatsAppMessage');
-  
+
   const options = {
     method: 'POST',
     headers: {
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNjA0ZjA4OWJlNzdjNWZkMTc5ZDY0NDcyNGRlMDZkNDRiNGUyOGY2NWE2NmE3NTA4YTAxZWU5YTczNjMxYjRkNWY5NmEzMDZhNjE4NTA1MzYiLCJpYXQiOjE2ODc0MTkwMTkuNjgyNjY0LCJuYmYiOjE2ODc0MTkwMTkuNjgyNjY4LCJleHAiOjQ4MTE1NTY2MTkuNjc0MDcyLCJzdWIiOiI2MDY4NTQiLCJzY29wZXMiOltdfQ.ecmX4Wlwhr1VllYQine5POyT4CTc3Zl41LIvetG8uOQuKD6jTQYVsKsWIb9PV1IkX5c1SHAAQ8LU6_rgIoD5DA',
-        'accept': 'application/json',
-        'content-type': 'application/json'
+      'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiNjA0ZjA4OWJlNzdjNWZkMTc5ZDY0NDcyNGRlMDZkNDRiNGUyOGY2NWE2NmE3NTA4YTAxZWU5YTczNjMxYjRkNWY5NmEzMDZhNjE4NTA1MzYiLCJpYXQiOjE2ODc0MTkwMTkuNjgyNjY0LCJuYmYiOjE2ODc0MTkwMTkuNjgyNjY4LCJleHAiOjQ4MTE1NTY2MTkuNjc0MDcyLCJzdWIiOiI2MDY4NTQiLCJzY29wZXMiOltdfQ.ecmX4Wlwhr1VllYQine5POyT4CTc3Zl41LIvetG8uOQuKD6jTQYVsKsWIb9PV1IkX5c1SHAAQ8LU6_rgIoD5DA',
+      'accept': 'application/json',
+      'content-type': 'application/json'
     },
     body: JSON.stringify({
       params: [{ key: '{{1}}', value: message }],
