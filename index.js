6f23eabbf44dfa62bec4a32ec39c955a322f11aa
Dylan Walls <dylanwalls@Dylans-MacBook-Pro-2.local>
2023-06-30 09:09:11 +0100

    remove crypto.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const CryptoJS = require('crypto-js');
 const winston = require('winston');
 
 const app = express();
