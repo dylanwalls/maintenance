62791e14378317a79027a75b12c381404031b3af
Dylan Walls <dylanwalls@dylans-mbp-2.home>
2023-07-04 09:15:07 +0100

    add Priscilla's number

--- index.js
+++ index.js
@@ -1,187 +1,3 @@
-// require('dotenv').config();
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const winston = require('winston');
-// const CryptoJS = require('crypto-js');
-// const axios = require('axios');
-// const CircularJSON = require('circular-json');
-
-// const app = express();
-// const PORT = process.env.PORT || 8080;
-// const signingSecret = process.env.SIGNING_SECRET || '';
-
-// // Configure the logger
-// const logger = winston.createLogger({
-//   transports: [
-//     new winston.transports.Console()
-//   ]
-// });
-
-// app.use(bodyParser.json()); // Parse request body as JSON
-
-// // Parse URLEncoded and save the raw body to req.rawBody
-// app.use(
-//   express.urlencoded({
-//     verify: (req, res, buf) => {
-//       req.rawBody = buf;
-//     },
-//     limit: '1mb',
-//     extended: true,
-//     type: 'application/x-www-form-urlencoded'
-//   })
-// );
-
-// app.get('/*', (req, res) => {
-//   res.send('Hello world');
-// });
-
-// // Listen for POST requests to '/webhook'
-// app.post('/webhook', (req, res) => {
-//   // Get the signature header
-//   const signature = req.header('Trengo-Signature') || '';
-//   // Get the raw request body
-//   const payload = req.rawBody || '';
-
-//   // Verify the signature
-//   if (verifySignature(payload, signature, signingSecret)) {
-//     // Signature is valid
-//     const ticket = req.body;
-//     logger.info('Received request payload:', JSON.stringify(ticket)); // Log the request payload
-
-//     // Check if the ticket object is null or empty
-//     if (!ticket) {
-//       logger.error('No ticket object received');
-//       res.status(400).json({ success: false, message: 'Invalid request payload' });
-//       return;
-//     }
-
-//     const message = ticket && ticket.message ? ticket.message : 'New ticket received';
-
-//     // Include the code for serializing the ticket object to remove circular references
-//     try {
-//       // Remove circular references before serializing
-//       const sanitizedData = CircularJSON.stringify(ticket);
-
-//       // Now you can use the sanitizedData in your application insights code
-//       // Customize the code below to perform your desired action with the sanitized data
-//       logger.info('Sanitized ticket data:', sanitizedData);
-//     } catch (error) {
-//       console.error('Failed to serialize payload:', error);
-//     }
-
-//     logger.info('Received ticket:', JSON.stringify(ticket)); // Log the received ticket object
-//     logger.info('Message:', message); // Log the message
-
-//     // Additional log statements
-//     logger.info('Street Address:', ticket.streetAddress);
-//     logger.info('Maintenance Description:', ticket.maintenanceDescription);
-//     logger.info('Your Name:', ticket.yourName);
-//     logger.info('Flat Letter:', ticket.flatLetter);
-//     logger.info('Contact Number:', ticket.contactNumber);
-//     logger.info('Photos:', ticket.photos);
-
-//     // Check if the ticket is assigned to the specific team
-//     if (ticket.team_id === '346034') {
-//       fetchTicketInformation(ticket.ticket_id)
-//         .then(ticketInfo => {
-//           logger.info('Ticket Information:', JSON.stringify(ticketInfo));
-//           // Remove the _httpMessage property from the TLSSocket object (if present)
-//           if (ticketInfo.socket && ticketInfo.socket._httpMessage) {
-//             delete ticketInfo.socket_httpMessage;
-//           }
-
-//           // Serialize the `ticketInfo` object to JSON.
-//           const json = JSON.stringify(ticketInfo);
-
-//           // Send the JSON response.
-//           res.json(json);
-//         })
-//         .catch(error => {
-//           logger.error('Failed to fetch ticket information:', error);
-//           res.status(500).json({ success: false, message: 'Failed to fetch ticket information' });
-//         });
-//     } else {
-//       res.json({ success: true, message: 'Ticket not assigned to the specific team' });
-//     }
-//   } else {
-//     // Invalid signature
-//     res.status(401).send('Unauthorized');
-//     logger.error('Invalid signature, please verify the signing secret');
-//   }
-// });
-
-// // Function to verify the Trengo signature
-// function verifySignature(payload, signature, signingSecret) {
-//   // Split the timestamp from the hash
-//   const signatureParts = signature.split(';');
-//   const timestamp = signatureParts[0];
-//   const signatureHash = signatureParts[1];
-
-//   // Generate a hash to compare with
-//   // 1. Get the raw digest bytes
-//   let hash = CryptoJS.HmacSHA256(timestamp + '.' + payload, signingSecret);
-
-//   // 2. Encode the raw bytes as hexadecimal digits
-//   hash = hash.toString(CryptoJS.enc.hex);
-
-//   // 3. Make the hexadecimal digits lowercase
-//   hash = hash.toLowerCase();
-
-//   // Compare our generated hash to the hash from the 'Trengo-Signature' header.
-//   // If they are the same, the signature is valid.
-//   return hash === signatureHash;
-// }
-
-// // Function to send WhatsApp message and return the ticket ID
-// function sendWhatsAppMessage(ticket) {
-//   // Your logic to send the WhatsApp message
-//   // ...
-
-//   // Log the ticket object
-//   logger.info('THE TICKET OBJECT IS:', ticket);
-//   // Return the ticket ID
-//   logger.info('THE TICKET ID IS:', ticket.ticket_id);
-//   return ticket.ticket_id;
-// }
-
-// // Function to fetch ticket information from Trengo API based on ticket ID
-// async function fetchTicketInformation(ticketId) {
-//   try {
-//     const response = await axios.get(`https://api.trengo.com/tickets/${ticketId}`, {
-//       headers: {
-//         Authorization: `Bearer ${process.env.TRENGO_API_TOKEN}`,
-//         'Content-Type': 'application/json'
-//       }
-//     });
-
-//     let ticketInfo = response.data;
-
-//     // Remove the circular reference in the `socket` object
-//     if (ticketInfo.socket && ticketInfo.socket._httpMessage) {
-//       delete ticketInfo.socket._httpMessage;
-//     }
-
-//     // Convert the ticketInfo object to JSON
-//     ticketInfo = JSON.stringify(ticketInfo);
-
-//     return ticketInfo;
-//   } catch (error) {
-//     throw error;
-//   }
-// }
-
-// app.listen(PORT, () => {
-//   logger.info(`Server is running on port ${PORT}`);
-// });
-
-
-
-
-
-
-
-
-
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -203,36 +19,6 @@ app.post('/webhook', async (req, res) => {
     team_name,
     assigned_to,
   } = req.body;
-
-  console.log('Received webhook:');
-  console.log('Ticket ID:', ticket_id);
-  console.log('User ID:', user_id);
-  console.log('User Name:', user_name);
-  console.log('User Email:', user_email);
-  console.log('Team ID:', team_id);
-  console.log('Team Name:', team_name);
-  console.log('Assigned To:', assigned_to);
-
-
-  // const {
-  //   ticket_id,
-  //   message,
-  //   streetAddress,
-  //   maintenanceDescription,
-  //   yourName,
-  //   flatLetter,
-  //   contactNumber,
-  //   photos,
-  //   team_id,
-  //   params,
-  // } = req.body;
-
-  // console.log('REQ.BODY:', req.body)
-  // console.log('Received ticket:', streetAddress); // Log the received ticket object
-
-  // const ticketMessage = `New ticket received from ${ticket_id} assigned to ${team_name}` || 'New ticket received';
-
-  // console.log('Message:', ticketMessage); // Log the message
 
   try {
     await sendWhatsAppMessage(ticket_id);
@@ -274,17 +60,7 @@ async function sendWhatsAppMessage(ticketId) {
     console.log('Flat:', flatLetter);
     console.log('Contact Number:', contactNumber);
 
-    const message_final = `New maintenance ticket received from ${yourName} at Flat ${flatLetter}, ${streetAddress}. Description: ${maintenanceDescription}. Please contact them for more information: ${contactNumber}.` || 'New ticket received';
-
-
-    //   2023-07-04T07:22:40.525056660Z   custom_data: {
-//   2023-07-04T07:22:40.525060661Z     'Maintenance description¨': 'Gf',
-//   2023-07-04T07:22:40.525065461Z     yourName: 'Jas',
-//   2023-07-04T07:22:40.525069461Z     streetAddress: 'Has',
-//   2023-07-04T07:22:40.525073461Z     flatLetter: 'H',
-//   2023-07-04T07:22:40.525077561Z     contactNumber: '0999999999',
-//   2023-07-04T07:22:40.525081561Z     photos: 'F'
-//   2023-07-04T07:22:40.531982805Z   },
+    const message_final = `${yourName} at Flat ${flatLetter}, ${streetAddress}. Contact: ${contactNumber}. Description: ${maintenanceDescription}.` || 'New ticket received';
 
     const sendMessageOptions = {
       method: 'POST',
@@ -295,7 +71,7 @@ async function sendWhatsAppMessage(ticketId) {
       },
       body: JSON.stringify({
         params: [{ key: '{{1}}', value: message_final }],
-        recipient_phone_number: '+27784130968',
+        recipient_phone_number: '+27829447959', // Priscilla's number
         hsm_id: '136514' // Replace with your WhatsApp template HSM ID
       })
     };
@@ -307,15 +83,6 @@ async function sendWhatsAppMessage(ticketId) {
     console.error(error);
     throw error;
     }
-
-  // try {
-  //   const response = await fetch('https://app.trengo.com/api/v2/wa_sessions', options);
-  //   const data = await response.json();
-  //   console.log('API Response:', data);
-  // } catch (error) {
-  //   console.error(error);
-  //   throw error;
-  // }
 }
 
 app.listen(PORT, () => {
